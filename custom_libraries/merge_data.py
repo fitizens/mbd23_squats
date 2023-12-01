d5140dee51e1ee3cb228ffaaecc34366bf83f519
joselerma97 <josemanuel@lermaserver.com>
2023-12-01 17:46:36 +0100

    Add merge_data

---
+++ custom_libraries/merge_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def merge_data(data: list):
+    time_series = []
+    timedelta = pd.Timedelta(milliseconds=0)
+    for info in data:
+        df = info["series"]
+        df.index = df.index + timedelta
+        time_series.append(df)
+        timedelta = pd.Timedelta(seconds=df.index.max().timestamp()) + pd.Timedelta(
+            milliseconds=1000
+        )
+    return pd.concat(time_series)
