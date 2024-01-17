fce330db08e25a859748deb860aed645d190e738
vivianaflorezc <131455064+vivianaflorezc@users.noreply.github.com>
2024-01-17 16:13:44 +0100

    Autolabel

--- custom_libraries/autolabel.py
+++ custom_libraries/autolabel.py
@@ -4,7 +4,7 @@ from custom_libraries.create_training_data_stats import create_custom_dataframe
 import pandas as pd
 import joblib 
 import warnings
-
+from pycaret.classification import *
 
 def label(df, num_picos:int, exercise:str):
     # Asegúrate de que la columna "peaks" esté presente y sea de tipo numérico
@@ -35,10 +35,11 @@ def label(df, num_picos:int, exercise:str):
                 modelo = joblib.load('modeloXGB.pkl')
                 X_estad = obtener_estadisticas(df_fraccionado)
                 y_pred = modelo.predict(X_estad)
+                
             elif exercise == 'PUSHUP':
                 modelo = joblib.load('logistic_model.pkl')
                 scaler = joblib.load('scaler_pushup.pkl')
-                data_info = create_training_data_stats(df_fraccionado)
+                data_info = create_training_data_stats(df_fraccionado,exercise)
                 data_custom = pd.DataFrame([data_info])
                 features = ['linAccZ_mean', 'linAccZ_std', 'linAccZ_median', 'linAccZ_min',
                             'linAccZ_max', 'linAccZ_range', 'linAccZ_quartile_25',
@@ -46,6 +47,65 @@ def label(df, num_picos:int, exercise:str):
                 X1 = data_custom[features].copy()
                 X_escal = scaler.transform(X1)
                 y_pred = modelo.predict(X_escal)
+                
+            elif exercise =='BURPEE':
+                pipeline = load_model(model_name="BURPEE")
+                data = create_training_data_stats(df_fraccionado,exercise)
+                data_custom = pd.DataFrame([data])
+                prediction = predict_model(pipeline, data_custom, raw_score=True)
+                y_pred = prediction.prediction_label.iloc[0]
+                
+            elif exercise == 'PULLUP':
+                modelo = joblib.load('pullup_model.pkl')
+                data_info = create_training_data_stats(df_fraccionado,exercise)
+                data_custom = pd.DataFrame([data_info])
+                features = ['linAccZ_mean', 'linAccZ_std', 'linAccZ_median',
+                            'linAccZ_kurtosis', 'linAccZ_quartile_25', 'linAccZ_iqr',
+                            'gyroX_quartile_25', 'gyroX_quartile_75', 'gyroX_iqr']
+                X1 = data_custom[features].copy()
+                y_pred = modelo.predict(X1)
+                
+            elif exercise == 'SITUP':
+                modelo = joblib.load('situp_model.pkl')
+                scaler = joblib.load('scaler_situp.pkl')
+                data_info = create_training_data_stats(df_fraccionado,exercise)
+                data_custom = pd.DataFrame([data_info])
+                features = ['linAccY_mean', 'linAccY_median', 'linAccY_min',
+                            'linAccY_quartile_25', 'linAccZ_quartile_75', 'gyroX_median',
+                            'magnX_skewness', 'magnY_skewness', 'magnZ_median']
+                X1 = data_custom[features].copy()
+                X_escal = scaler.transform(X1)
+                y_pred = modelo.predict(X_escal)
+                
+            elif exercise == 'SQUAT JUMP':
+                modelo = joblib.load('jump_model.pkl')
+                scaler = joblib.load('scaler_jump.pkl')
+                data_info = create_training_data_stats(df_fraccionado,exercise)
+                data_custom = pd.DataFrame([data_info])
+                features = ['linAccZ_std', 'linAccZ_min', 'linAccZ_quartile_25',
+                            'linAccZ_quartile_75', 'linAccZ_iqr', 'gyroX_std', 'gyroX_min',
+                            'gyroX_quartile_25', 'gyroX_iqr']
+                X1 = data_custom[features].copy()
+                X_escal = scaler.transform(X1)
+                y_pred = modelo.predict(X_escal)
+                
+            elif exercise =='JUMPING JACK':
+                pipeline = load_model(model_name="jack_model")
+                data = create_training_data_stats(df_fraccionado,exercise)
+                data_custom = pd.DataFrame([data])
+                prediction = predict_model(pipeline, data_custom, raw_score=True)
+                y_pred = prediction.prediction_label.iloc[0]
+                
+            elif exercise =='DUMBBELL':
+                pipeline = load_model(model_name="DUMBBELL")
+                data = create_training_data_stats(df_fraccionado,exercise)
+                data_custom = pd.DataFrame([data])
+                prediction = predict_model(pipeline, data_custom, raw_score=True)
+                y_pred = prediction.prediction_label.iloc[0]
+                
+            else:
+                raise ValueError("Please review the written exercise. Exercise must be: SQUAT, PUSHUP, BURPEE, PULLUP,                                         SITUP, SQUAT JUMP, JUMPING JACK or DUMBBELL")
+
 
             # Almacenar la predicción en la lista como un diccionario
             resultados.append({'Prediccion': y_pred, 'Inicio_Ventana': inicio_ventana, 'Fin_Ventana': fin_ventana})
--- custom_libraries/create_training_data_stats.py
+++ custom_libraries/create_training_data_stats.py
@@ -4,35 +4,84 @@ def create_custom_dataframe(series):
     return df
 
 
-def create_training_data_stats(df):
+
+def create_training_data_stats(df, exercise):
     stats_dict = {}
     series = create_custom_dataframe(df)
-    
+
     for column in series.columns:
         mean = series[column].mean()
         std = series[column].std()
         median = series[column].median()
-        skewness = series[column].skew()
-        kurtosis = series[column].kurtosis()
-        min_val = series[column].min()
-        max_val = series[column].max()
-        range_val = max_val - min_val
-        quartile_25 = np.percentile(series[column], 25)
-        quartile_75 = np.percentile(series[column], 75)
-        iqr = quartile_75 - quartile_25
-    
 
         stats_dict[f"{column}_mean"] = mean
         stats_dict[f"{column}_std"] = std
         stats_dict[f"{column}_median"] = median
-        stats_dict[f"{column}_skewness"] = skewness
-        stats_dict[f"{column}_kurtosis"] = kurtosis
-        stats_dict[f"{column}_min"] = min_val
-        stats_dict[f"{column}_max"] = max_val
-        stats_dict[f"{column}_range"] = range_val
-        stats_dict[f"{column}_quartile_25"] = quartile_25
-        stats_dict[f"{column}_quartile_75"] = quartile_75
-        stats_dict[f"{column}_iqr"] = iqr
+
+        if exercise == 'PUSHUP' or exercise == 'PULLUP' or exercise == 'SITUP' or exercise == 'SQUAT JUMP':
+            skewness = series[column].skew()
+            kurtosis = series[column].kurtosis()
+            min_val = series[column].min()
+            max_val = series[column].max()
+            range_val = max_val - min_val
+            quartile_25 = np.percentile(series[column], 25)
+            quartile_75 = np.percentile(series[column], 75)
+            iqr = quartile_75 - quartile_25
+
+
+            stats_dict[f"{column}_skewness"] = skewness
+            stats_dict[f"{column}_kurtosis"] = kurtosis
+            stats_dict[f"{column}_min"] = min_val
+            stats_dict[f"{column}_max"] = max_val
+            stats_dict[f"{column}_range"] = range_val
+            stats_dict[f"{column}_quartile_25"] = quartile_25
+            stats_dict[f"{column}_quartile_75"] = quartile_75
+            stats_dict[f"{column}_iqr"] = iqr
+
+        elif exercise == 'BURPEE':
+            quartile_25 = np.percentile(series[column], 25)
+            quartile_75 = np.percentile(series[column], 75)
+            iqr = quartile_75 - quartile_25
+
+            max_val = series[column].max()
+            stats_dict[f"{column}_quartile_25"] = quartile_25
+            stats_dict[f"{column}_quartile_75"] = quartile_75
+            stats_dict[f"{column}_iqr"] = iqr
+            stats_dict[f"{column}_max"] = max_val
         
-         
+        elif exercise == 'JUMPING JACK':
+            skewness = series[column].skew()
+            kurtosis = series[column].kurtosis()
+            variance = series[column].var()  # Calculate variance
+            rms = np.sqrt(np.mean(series[column]**2))
+            
+            stats_dict[f"{column}_median"] = median
+            stats_dict[f"{column}_skewness"] = skewness
+            stats_dict[f"{column}_kurtosis"] = kurtosis
+            stats_dict[f"{column}_variance"] = variance  
+            stats_dict[f"{column}_rms"] = rms
+            
+        elif exercise == 'DUMBBELL':
+            skewness = series[column].skew()
+            kurtosis = series[column].kurtosis()
+            
+            stats_dict[f"{column}_skewness"] = skewness
+            stats_dict[f"{column}_kurtosis"] = kurtosis
+
+
     return stats_dict
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
